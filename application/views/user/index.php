58aa510d8ba49cc3dd07bd9457acae48707677fc
wp2Luthfi <luthfinurhastomo2865@gmail.com>
2024-06-25 15:24:46 +0700

    fix bug

---
+++ application/views/user/index.php
@@ -0,0 +1,7 @@
+<br><br><br>
+    <div class="container text-center" style="margin: 2em auto;">
+      <div class="jumbotron">
+        <h1 class="display-3">Home</h1>
+        <p class="lead">Belum Ada Apa Apa</p>
+    </div>
+  </div>
